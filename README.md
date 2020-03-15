7a3966808d5ca9e5336f59e414079aa0378444b6
Jill Marcum <jill.marcum@gmail.com>
2020-03-15 14:56:18 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+SmartBrain: a facial recognition web app using the Clarifai API, built using React, Node.js, Express, and PostgreSQL.
---
+++ src/App.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Logo from './components/logo/Logo';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
+import Navigation from './components/navigation/Navigation';
+import './App.css';
+
+
+
+const particlesOptions ={
+      "particles": {
+          "number": {
+              "value": 50
+          },
+          "size": {
+              "value": 3
+          },
+          "density": {
+            "enable": true,
+            "value-area": 800
+          }
+      },
+      "interactivity": {
+          "events": {
+              "onhover": {
+                  "enable": true,
+                  "mode": "repulse"
+              }
+          }
+      }
+}
+
+const initialState = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState
+    }
+
+
+loadUser = (data) => {
+  this.setState({
+    user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }
+  })
+}
+
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+
+    //missing the Clarifai API cal that was moved to the back end.
+
+                fetch('https://quiet-gorge-98089.herokuapp.com/imageurl', {
+                  method: 'post',
+                  headers: {'Content-Type': 'application/json'},
+                  body: JSON.stringify({
+                    input: this.state.input
+                  })
+                })
+              .then(response => response.json())
+
+    //the preceding was in place of the Clarifai API call, but I feel the last line might duplicate the what comes next and cause an error somehow.
+
+    .then(response => {
+      if(response){
+        fetch('https://quiet-gorge-98089.herokuapp.com/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+          id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, {entries: count}))
+        })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err));
+  }
+
+
+  onRouteChange = (route) => {
+    if(route === 'signout'){
+      this.setState(initialState)
+    } else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={ particlesOptions }
+        />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route ==='home'
+          ? <div>
+              <Logo />
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
+              <ImageLinkForm
+                onInputChange = {this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box = {box} imageUrl={imageUrl}/>
+            </div>
+          : (
+              route ==='signin'
+              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
+
