9d7dfb30ea5e9e44480d5ae67734f3fbb5f254c2
Jill Marcum <jill.marcum@gmail.com>
2020-07-25 12:40:28 -0700

    add profile modal component and bind to root index.html

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './Modal.css';
+
+const modalRoot = document.getElementById('modal-root');
+
+class Modal extends React.Component {
+
+}
+
+export default Modal;
