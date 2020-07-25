dfc0a289c73625d3abc204d6d9c7c744fd95f6f0
Jill Marcum <jill.marcum@gmail.com>
2020-07-24 17:58:09 -0700

    add custom profile dropdown (reactstrap and hooks)

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class ProfileIcon extends React.Component {
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+const ProfileIcon = (props) => {
+
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen(prevState => !prevState);
+
+  return (
+    <Dropdown isOpen={dropdownOpen} toggle={toggle} className='pa4 tc'>
+      <DropdownToggle
+        tag="span"
+        data-toggle="dropdown"
+        aria-expanded={dropdownOpen}
+      >
+        <img
+            src="http://tachyons.io/img/logo.jpg"
+            class="br-100 ba h3 w3 dib" alt="avatar"/>
+        </DropdownToggle>
+      <DropdownMenu
+        className='b--transparent shadow-5'
+        style={{marginTop: '10px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
+        <DropdownItem>View Profile</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Sign Out</DropdownItem>
+        <DropdownItem disabled>Action (disabled)</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
+
+export default ProfileIcon;
+
+/*class ProfileIcon extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dropdownOpen = false,
+      dropdownOpen: false,
     }
   }
 
@@ -18,5 +51,5 @@ class ProfileIcon extends React.Component {
     );
   }
 }
+*/
 
-export default ProfileIcon;
