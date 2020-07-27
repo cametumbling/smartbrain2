4b02f7d9b6debba27ccc2c3ca25cd08a37c01c5c
Jill Marcum <jill.marcum@gmail.com>
2020-07-27 16:32:09 -0700

    add onFormChange()

--- src/App.js
+++ src/App.js
@@ -141,6 +141,7 @@ class App extends Component {
                 <Profile
                   isProfileOpen={isProfileOpen}
                   toggleModal={this.toggleModal}
+                  loadUser = {this.loadUser}
                   user={user}/>
           </Modal>
         }
