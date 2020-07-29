b5830377caaff6838415808a8cab893b0b40ff51
Jill Marcum <jill.marcum@gmail.com>
2020-07-29 16:50:06 -0700

    check if user has token in sessionStorage

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,6 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    //fetch('https://quiet-gorge-98089.herokuapp.com/register', {
     fetch('http://localhost:3000/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
